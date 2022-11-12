6d67ee91a832366f02b3a845f520fdb24057b5b9
Dilan <dilan@dilanxd.com>
2022-11-12 16:05:47 -0600

    Updates

--- _app/immutable/pages/paper-nu.svelte-0bab6064.js
+++
@@ -1,9 +0,0 @@
-import{S as N,i as P,s as b,a as w,x as l,u as q,G as C,h as $,c as S,y as _,v as y,b as m,z as c,f as g,t as v,C as x}from"../chunks/index-09afa4d5.js";import{N as F,H,F as M}from"../chunks/Navigation-84cd0ae1.js";import{S as z}from"../chunks/Space-8e616423.js";function D(d){let a;return{c(){a=q("Paper")},l(t){a=y(t,"Paper")},m(t,o){m(t,a,o)},d(t){t&&$(a)}}}function E(d){let a,t,o,s,i,r,p,f,u;return t=new F({}),s=new H({props:{h1:!0,$$slots:{default:[D]},$$scope:{ctx:d}}}),r=new z({props:{lg:!0}}),f=new M({}),{c(){a=w(),l(t.$$.fragment),o=w(),l(s.$$.fragment),i=q(`
-
-More info coming soon
-
-`),l(r.$$.fragment),p=w(),l(f.$$.fragment),this.h()},l(e){C('[data-svelte="svelte-1a8qfub"]',document.head).forEach($),a=S(e),_(t.$$.fragment,e),o=S(e),_(s.$$.fragment,e),i=y(e,`
-
-More info coming soon
-
-`),_(r.$$.fragment,e),p=S(e),_(f.$$.fragment,e),this.h()},h(){document.title="paper.nu - Dilan Nair"},m(e,n){m(e,a,n),c(t,e,n),m(e,o,n),c(s,e,n),m(e,i,n),c(r,e,n),m(e,p,n),c(f,e,n),u=!0},p(e,[n]){const h={};n&1&&(h.$$scope={dirty:n,ctx:e}),s.$set(h)},i(e){u||(g(t.$$.fragment,e),g(s.$$.fragment,e),g(r.$$.fragment,e),g(f.$$.fragment,e),u=!0)},o(e){v(t.$$.fragment,e),v(s.$$.fragment,e),v(r.$$.fragment,e),v(f.$$.fragment,e),u=!1},d(e){e&&$(a),x(t,e),e&&$(o),x(s,e),e&&$(i),x(r,e),e&&$(p),x(f,e)}}}class A extends N{constructor(a){super(),P(this,a,null,E,b,{})}}export{A as default};
