a20487bcf50bd9eb4ebdc7bcfc0fdbf3d38b2cb9
Dilan <dilan@dilanxd.com>
2022-11-18 18:36:08 -0600

    add 404

--- svelte.config.js
+++ svelte.config.js
@@ -11,9 +11,8 @@ const config = {
     adapter: adapter({
       pages: 'build',
       assets: 'build',
-      fallback: null,
+      fallback: '404.html',
       precompress: false,
-      strict: true,
     }),
 
     trailingSlash: 'always',
