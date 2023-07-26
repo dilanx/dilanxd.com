c8c944b0f2432212a32e95621b68cd49b2d1df37
Dilan <dilan@dilanxd.com>
2023-07-26 15:48:18 -0700

    add genredir script for use

--- generate-redirects.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-while read one && read two; do
-  echo "generate-redirects: mapping /$one to '$two'"
-  html="<!DOCTYPE html><html lang=\"en\"><head><meta charset=\"UTF-8\" /><meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\" /><title>Redirecting...</title><script>window.location.replace(\`$two\`);</script></head></html>"
-  mkdir -p build/$one
-  echo "$html" > build/$one/index.html
-done < redirects.txt
---
+++ static/genredir.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+# Generate redirects from a text file
+
+# Run locally like this:
+# bash -c "$(curl -fsSL dilanxd.com/genredir.sh)" < REDIRECT_FILE_NAME
+# 
+# Specify output directory like this:
+# OUT=build bash -c "$(curl -fsSL dilanxd.com/genredir.sh)" < REDIRECT_FILE_NAME
+# By default, output directory is 'build'
+
+# Redirect files should look like this:
+# path1 target1
+# path2 target2
+# ...
+# 
+# Example:
+# guide https://example.com/guide/
+# blog https://example.org/blog/
+
+OUT="${OUT:-build}"
+
+echo "genredir: using output directory '$OUT'"
+while read line; do
+  read -ra arr <<< "$line"
+  one=${arr[0]}
+  two=${arr[1]}
+
+  if [ -z "$one" ]; then
+    continue
+  fi
+
+  if [ -z "$two" ]; then
+    echo "genredir: no redirect target for '/$one', skipping"
+    continue
+  fi
+  
+  html="<!DOCTYPE html><html lang=\"en\"><head><meta charset=\"UTF-8\" /><meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\" /><title>Redirecting...</title><script>window.location.replace(\`$two\`);</script></head></html>"
+  mkdir -p $OUT/$one
+  echo "$html" > $OUT/$one/index.html
+  
+  echo "genredir: mapped /$one to '$two'"
+done
