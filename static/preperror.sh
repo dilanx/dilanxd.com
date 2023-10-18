be3c9bba033cdcbecf1a5bcc210d50a613aa3e46
Dilan <dilan@dilanxd.com>
2023-10-17 23:19:03 -0500

    site update

--- static/preperror.sh
+++ static/preperror.sh
@@ -11,7 +11,7 @@
 OUT="${OUT:-build}"
 
 echo "preperror: generating 404"
-html="<!DOCTYPE html><html lang=\"en\"><head><meta charset=\"UTF-8\"><meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\"><title>404 - dilanxd.com site</title><link rel=\"stylesheet\" href=\"https://www.dilanxd.com/fonts/fonts.css\"><link rel=\"stylesheet\" href=\"https://www.dilanxd.com/preperror.css\"></head><body><main><div class=\"heading\"><img src=\"https://www.dilanxd.com/logo.svg\" alt=\"dilanxd.com logo\" /><div class=\"divider\"></div><h1>404</h1></div><p>The page you requested was not found.</p></main></body></html>"
+html="<!DOCTYPE html><html lang=\"en\"><head><meta charset=\"UTF-8\"><meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\"><title>404 - a dilanxd.com site</title><link rel=\"stylesheet\" href=\"https://www.dilanxd.com/fonts/fonts.css\"><link rel=\"stylesheet\" href=\"../static/preperror.css\"></head><body><main><div class=\"heading\"><a href=\"https://www.dilanxd.com\"><img src=\"https://www.dilanxd.com/logo.svg\" alt=\"dilanxd.com logo\" /></a><div class=\"divider\"></div><h1>404</h1></div><p>The page you requested could not be found.</p></main></body></html>"
 mkdir -p $OUT
 echo "$html" > $OUT/404.html
 echo "preperror: generated 404"
