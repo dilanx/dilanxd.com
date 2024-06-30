e8dda9cb9c47fcdf604f740a4293570f90df390e
Dilan <dilan@dilanxd.com>
2024-06-30 00:04:54 -0400

    update redirects

--- src/routes/paper/changelog/+page.svelte
+++
@@ -1,9 +0,0 @@
-<script lang="ts">
-  import Redirect from '$lib/components/Redirect.svelte';
-</script>
-
-<svelte:head>
-  <title>Paper Change Log - Dilan Nair</title>
-</svelte:head>
-
-<Redirect to="https://github.com/dilanx/paper.nu/blob/main/CHANGELOG.md" />
